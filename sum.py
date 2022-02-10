42c7c62720b086f42471f324429de2ed6c2bd61e
Sachin Patel <85048630+sach-1401@users.noreply.github.com>
2022-02-10 09:34:54 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,16 @@
+# Python program to add two numbers
+
+# Given two numbers num1 and num2. 
+# The task is to write a Python program to find the addition of these two numbers.
+
+# Enter first number
+num1=int(input('enter first number :'))
+
+# Enter second number
+num2=int(input('enter second number :'))
+
+# Sum of two input number
+sum=num1+num2
+
+# here we call final output
+print(sum)
